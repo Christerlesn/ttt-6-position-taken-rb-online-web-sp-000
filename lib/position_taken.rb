4036979baa73785328e1768c3908d1d82c77cc53
Christerle Nounoute <christerlesn@gmail.com>
2020-08-15 16:23:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,3 @@
 def position_taken?(board,index)
   if board[index] == " "
     false
-  elsif board[index] == ""
-    false
-  elsif  board[index] == nil
-    false
-  else   board[index] == "X" || "O"
-    true
-
-  end
-end
