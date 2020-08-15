d619a8e5f62e452f01c7f7d848740fc68f29ca46
Christerle Nounoute <christerlesn@gmail.com>
2020-08-15 15:25:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,4 @@
 def position_taken?(board, index = 0)
-  if board = ["", " ", " ", " ", " ", " ", " ", " ", " "] == index
-
-  else board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"] == index
+board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"] == index
 
 end
